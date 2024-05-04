f0c6f0c993df3926d4ecf9a4ce052f7af994a2b3
Emma <kilokoemma175@gmail.com>
2024-05-04 12:58:10 -0700

    task3

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  let checkedAmenities = {};
+  $(document).on('change', "input[type='checkbox']", function () {
+    if (this.checked) {
+      checkedAmenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete checkedAmenities[$(this).data('id')];
+    }
+    let lst = Object.values(checkedAmenities);
+    if (lst.length > 0) {
+      $('div.amenities > h4').text(Object.values(checkedAmenities).join(', '));
+    } else {
+      $('div.amenities > h4').html('&nbsp;');
+    }
+  });
+  $.get('http://0.0.0.0:5001/api/v1/status/').click(function () {
+    if ($(this).prop('checked')) {
+      amenityIds[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else if (!$(this).prop('checked')) {
+      delete amenityIds[$(this).attr('data-id')];
+    }
+    if (Object.keys(amenityIds).length === 0) {
+      $('div.amenities h4').html('&nbsp');
+    } else {
+      $('div.amenities h4').text(Object.values(amenityIds).join(', '));
+    }
+  });
+});
